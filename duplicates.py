9c85771860c4e5fd24b16c9773a50d0a8e64026f
hxndgtlmg <hxndgtlmg@users.noreply.github.com>
2016-10-23 01:22:35 +0300

    Написан алгоритм

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,22 @@
+import os
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def crawl(path):
+    files_dict = {}
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        for file in filenames:
+            file_path = os.path.join(dirpath, file)
+            if files_dict.get(file) is None:
+                files_dict[file] = {'size': os.path.getsize(file_path),
+                                    'path': file_path}
+            elif os.path.getsize(file_path) == files_dict[file]['size']:
+                print('Найдено совпадение!\n'
+                      '{0}\n'
+                      '{1}\n'
+                      'Размер: {2}'.format(file_path,
+                                           files_dict[file]['path'],
+                                           files_dict[file]['size']))
 
 
 if __name__ == '__main__':
-    pass
+    crawl(input('Введите путь к папке: '))
